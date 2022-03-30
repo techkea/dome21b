904abff18b8ec7960a4716e2adf6ec6380f49819
clbokea <clbo@kea.dk>
2022-03-30 09:29:00 +0200

    initial commit

---
+++ src/main/java/com/example/demo21b/Demo21bApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo21b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo21bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo21bApplication.class, args);
+    }
+
+}
